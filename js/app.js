12814906e89d427c56d862070b10779c2909d4e4
Krystian Jagoda <kristian2x@gmail.com>
2017-02-20 16:01:00 +0100

    added fonts, images, section1

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
-$(function(){
+$(function() {
+
+    var menu = $('.nav_menu');
+    var links = menu.find('a');
 
 
+    links.on("click", function(e) {
+        var target = $($(this).attr("href"));
 
-
-
-
-
-
-
-
+        if (target.length) {
+            event.preventDefault();
+            $("html, body").animate({
+                scrollTop: target.offset().top
+            }, 2000);
+        }
+    });
 
 
 
