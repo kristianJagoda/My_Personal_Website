f4313afeb1673157e1340c6bec5cf9c5c3d979f6
Krystian Jagoda <kristian2x@gmail.com>
2017-02-21 20:40:11 +0100

    Adding CSS animation to the skills section

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 $(function() {
-    //Slide Menu
-    var menu = $('.menu');
-    var links = menu.find('a');
+
+    //Smooth Scrolling Menu
+    (function() {
+
+        var menu = $('.menu');
+        var links = menu.find('a');
 
 
-    links.on("click", function(e) {
-        var target = $($(this).attr("href"));
+        links.on("click", function(e) {
+            var target = $($(this).attr("href"));
 
-        if (target.length) {
-            event.preventDefault();
-            $("html, body").animate({
-                scrollTop: target.offset().top
-            }, 2000);
-        }
-    });
+            if (target.length) {
+                event.preventDefault();
+                $("html, body").animate({
+                    scrollTop: target.offset().top
+                }, 2000);
+            }
+        });
 
+    })();
 
 
 
